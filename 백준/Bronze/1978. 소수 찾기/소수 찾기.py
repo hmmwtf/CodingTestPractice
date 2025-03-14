2f1ce6f0aeba51e799f7ae6632f978f3ad52f2e8
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-03-14 21:05:08 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,17 +1,15 @@
 def is_prime(n):
-    if n <= 1:
+    if n < 2:
         return 0
-    elif n == 2:
-        return 1
     else:
-        cnt =  0
-        for i in range(2, n+1):
+        cnt = 0
+        for i in range(2, int(n**(0.5)+1)):
             if n % i == 0:
                 cnt += 1
-        if cnt == 1:
-            return 1
-        else:
+        if cnt > 0:
             return 0
+        else:
+            return 1
 
 n = int(input())
 cnt = 0
@@ -21,4 +19,3 @@ for i in arr:
         cnt += 1
 
 print(cnt)
-    
