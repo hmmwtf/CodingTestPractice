177191f6685fa2d70f7ffaba593d41da5beb7f01
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-03-16 23:35:42 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,11 +1,30 @@
-from itertools import combinations
+arr = []
 
-arr = [] 
 for i in range(9):
     arr.append(int(input()))
 
-for num in list(combinations(arr, 7)):
-    if sum(num) == 100:
-        for shorted in sorted(num):
-            print(shorted)
-        break
+total = sum(arr)
+
+solved = False
+
+for i in range(len(arr)):
+    fake1 = arr[i]
+    for j in range(len(arr)):
+        if i == j:
+            continue
+        else:
+            fake2 = arr[j]
+            for k in range(len(arr)):
+                if total - fake1 - fake2 == 100:
+                    arr.remove(fake1)
+                    arr.remove(fake2)
+                    solved = True
+                    break
+        if solved == True:
+            break
+    if solved == True:
+        break               
+
+arr.sort()
+for i in range(7):
+    print(arr[i])
