af2ec6838d4554ca6825c7505fc3cdb30e5e1078
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-03-20 22:21:24 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,9 +1,13 @@
-k = int(input())
+import sys
+
+input = sys.stdin.readline
+
+k = int(input().strip())
 
 arr = []
 
 for i in range(k):
-    val = int(input())
+    val = int(input().strip())
     if val == 0:
         arr.pop()
     else:
