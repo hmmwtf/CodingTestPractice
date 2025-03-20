11b1dd0b44b5768c4a6c86a19f9b3200eadab08f
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-03-20 22:02:04 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.read
+
 def push(arr, val):
     arr.append(val)
 
@@ -15,26 +18,32 @@ def is_empty(arr):
         print(1)
     else:
         print(0)
-    
+
 def top(arr):
     if len(arr) == 0:
         print(-1)
     else:
         print(arr[-1])
 
-n = int(input())
-arr = []
-for i in range(n):
-    order = input().split()
-    if order[0] == 'push':
-        push(arr, order[1])
-    elif order[0] == 'pop':
-        pop(arr)
-    elif order[0] == 'size':
-        size(arr)
-    elif order[0] == 'empty':
-        is_empty(arr)
-    elif order[0] == 'top':
-        top(arr)
-    else:
-        print("Invalid Input")
+def main():
+    data = input().splitlines()  
+    n = int(data[0])  
+    arr = []
+
+    for i in range(1, n + 1): 
+        order = data[i].split()
+        if order[0] == 'push':
+            push(arr, order[1])
+        elif order[0] == 'pop':
+            pop(arr)
+        elif order[0] == 'size':
+            size(arr)
+        elif order[0] == 'empty':
+            is_empty(arr)
+        elif order[0] == 'top':
+            top(arr)
+        else:
+            print("Invalid Input")
+
+if __name__ == "__main__":
+    main()
