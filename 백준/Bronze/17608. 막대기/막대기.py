73e2f2c321a8034de5eea8d70e2469d0c4d268f0
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-03-21 10:18:15 +0900

    [Bronze II] Title: 막대기, Time: 92 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -6,13 +6,12 @@ n = int(input().strip())
 
 height = []
 
-cnt = 1
-
 for i in range(n):
     height.append(int(input().strip()))
 
 view = height[-1]
-for i in range(len(height)-2, -1, -1):
+cnt = 1
+for i in range(len(height)-1, -1, -1):
     if height[i] > view:
         cnt += 1
         view = height[i]
