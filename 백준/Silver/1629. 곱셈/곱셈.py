dd0901849416b7350a6b3f0e748a780618f6964c
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-03-25 11:10:21 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,4 +1,14 @@
 import sys
 input = sys.stdin.readline
+
+def mod_pow(a, b, mod):
+    if b == 0:
+        return 1
+    temp = mod_pow(a, b // 2, mod)
+    temp = (temp * temp) % mod
+    if b % 2 == 0:
+        return temp
+    else:
+        return (temp * a) % mod
 a,b,c = map(int, input().strip().split())
-print(pow(a, b, c))
+print(mod_pow(a, b, c))
