11dd8a8c54c675be0f738b9a18067773bb2d946b
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-04-03 19:01:04 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def fibo(n):
+    arr = []
+    arr.append(0)
+    arr.append(1)
+    
+    for i in range(2, n+1):
+        arr.append(arr[i-1] + arr[i-2])
+    
+    return arr[n]
+
+n = int(input().strip())
+print(fibo(n))
