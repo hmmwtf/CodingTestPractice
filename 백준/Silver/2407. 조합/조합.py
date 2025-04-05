81560d4d5524e70c913e71d269bf8462dc0ba472
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-04-05 14:42:37 +0900

    [Silver III] Title: 조합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,4 +1,17 @@
-import sys, math
+import sys
 input = sys.stdin.readline
+
 n, m = map(int, input().strip().split())
-print(math.comb(n, m))
+
+dp = [[0] * (m + 1) for _ in range(n+1)]
+
+for i in range(0, n + 1):
+    dp[i][0] = 1
+    if i <= m:
+        dp[i][0] = 1
+        
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+        
+print(dp[n][m])
