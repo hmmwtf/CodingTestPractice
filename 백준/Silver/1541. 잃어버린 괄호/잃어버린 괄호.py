39baa6cc613d2fbade187d6fd194cf97016c81bd
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-04-09 11:35:55 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 36944 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,12 @@
+import sys, re
+input = sys.stdin.readline
+
+expression = input().strip()
+parts = expression.split('-')
+
+result = sum(map(int, parts[0].split('+')))
+
+for part in parts[1:]:
+    result -= sum(map(int, part.split('+')))
+
+print(result)
