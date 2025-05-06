e8c0da0fdc9b511adc0cfa0c5fe35ee2cfd317df
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-05-06 15:30:23 +0900

    [Silver II] Title: 아기 상어 2, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -2,39 +2,29 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+def solve():
+    ans = 1
+    
+    while sharks:
+        i, j = sharks.popleft()
+        for di, dj in directions:
+            ii, jj = i + di, j + dj
+            if 0 <= ii < N and 0 <= jj < M and area[ii][jj] == 0:
+                area[ii][jj] = area[i][j] + 1  
+                sharks.append((ii, jj))         
+                ans = max(ans, area[ii][jj])    
 
-dif = [[0]*m for _ in range(n)]
-max_dist = 0
+    print(ans - 1)
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] != 0:
-            continue
+N, M = map(int, input().split())
+area = [list(map(int, input().split())) for _ in range(N)]
 
-        visited = [[False]*m for _ in range(n)]
-        q = deque([(i, j, 0)])
-        visited[i][j] = True
+directions = [(-1, -1), (-1, 0), (-1, 1), ( 0, -1), ( 0, 1), ( 1, -1), ( 1, 0), ( 1, 1)]  
 
-        found = False
-        while q and not found:
-            x, y, dist = q.popleft()
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if not (0 <= nx < n and 0 <= ny < m):
-                    continue
-                if visited[nx][ny]:
-                    continue
-                visited[nx][ny] = True
+sharks = deque()
+for i in range(N):
+    for j in range(M):
+        if area[i][j] == 1:
+            sharks.append((i, j))
 
-                if graph[nx][ny] == 1:
-                    dif[i][j] = dist + 1
-                    max_dist = max(max_dist, dist + 1)
-                    found = True
-                    break
-                else:
-                    q.append((nx, ny, dist + 1))
-
-print(max_dist)
+solve()
